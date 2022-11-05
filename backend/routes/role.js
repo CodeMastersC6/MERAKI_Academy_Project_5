2e0a24480f7064a1be601b3111d2196ba3077622
Anashawi <anas.moh.alhawi@gmail.com>
2022-11-05 21:18:05 +0200

    role&&permission &&role_permission

---
+++ backend/routes/role.js
@@ -0,0 +1,12 @@
+const express = require("express");
+//controllers
+const {
+    createNewRole,createNewPermission,createNewRolePermission
+  } = require("../controllers/role");
+  
+  const roleRouter = express.Router();
+  
+  roleRouter.post("/", createNewRole);
+  roleRouter.post("/permission", createNewPermission);
+  roleRouter.post("/role_permission", createNewRolePermission);
+  module.exports = roleRouter;
